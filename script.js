5ca00475911225f9c1f01c0a8cdb7166b5ea608d
Jake Gordon <github@jakeg.co.uk>
2017-06-17 15:40:30 +0100

    full screen canvas to start with

---
+++ script.js
@@ -0,0 +1,6 @@
+(function() {
+  var canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.appendChild(canvas);
+})();
